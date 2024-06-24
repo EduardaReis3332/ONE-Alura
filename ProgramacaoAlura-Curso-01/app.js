aa226c7cb35638ecec1e95a7942001464edc8bad
Eduarda Reis de Oliveira <eduarda.oliveira@sipam.gov.br>
2024-06-24 16:08:49 -0300

    upgrade app.js

--- ProgramacaoAlura-Curso-01/app.js
+++ ProgramacaoAlura-Curso-01/app.js
@@ -1,5 +1,6 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -7,7 +8,7 @@ let tentativas = 1;
 // enquanto chute não for igual ao numero secréto
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     // se o chute for igual ao numero secreto, exiba a mensagem:
     if (chute == numeroSecreto) {
