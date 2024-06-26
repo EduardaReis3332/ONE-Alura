07022d0677bf09ced819292abd5aad32def5a862
Eduarda Reis de Oliveira <eduarda.oliveira@sipam.gov.br>
2024-06-26 17:39:11 -0300

    upgrade Curso-02;

--- Curso-02/app.js
+++ Curso-02/app.js
@@ -1,17 +1,19 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do Número Secreto';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+let numeroSecreto = gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto')
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log('botão foi clicado');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+    
 }
