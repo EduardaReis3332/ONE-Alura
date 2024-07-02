33e3207874223544964780a8daad3b0d22b7a39f
Eduarda Reis de Oliveira <eduarda.oliveira@sipam.gov.br>
2024-07-02 17:22:58 -0300

    upgrade app.js & index.html & style.css do Desafios Curso-02

--- Desafios/Desafios Curso-02/app.js
+++ Desafios/Desafios Curso-02/app.js
@@ -33,4 +33,34 @@ let media = (num1, num2, num3) => console.log((num1 + num2 + num3)/3);
 
 let maior = (x, y) => console.log(x > y ? x : y);
 
-let multEleMesmo = (num) => console.log(num ** 2)
+let multEleMesmo = (num) => console.log(num ** 2);
+
+let imc = (altura, peso) => alert(peso / (altura ** 2));
+
+let conversaoDolar = (dolar) => alert(`R$${dolar * 4.80}`);
+
+function areaPerimetro(altura, largura) {
+    
+    let area = altura * largura;
+
+    let perimetro = 2 * (altura + largura);
+
+    return alert(`A sala retangular tem como área: ${area} e perímetro: ${perimetro}.`);
+}
+
+function areaPerimetroCirculo(raio) {
+    
+    let pi = 3.14;
+
+    let area = pi * (raio ** 2);
+
+    let perimetro = 2 * pi * raio;
+
+    return alert(`A sala circula tem como área: ${area} e perímetro: ${perimetro}.`);
+}
+
+let tabuada = () => {
+    let numTab = parseInt(prompt('Digite um número')); 
+    
+    return alert(`2 X ${numTab}: ${numTab*2}\n3 X ${numTab}: ${numTab*3}\n4 X ${numTab}: ${numTab*4}\n5 X ${numTab}: ${numTab*5}\n6 X ${numTab}: ${numTab*6}\n7 X ${numTab}: ${numTab*7}\n8 X ${numTab}: ${numTab*8}\n9 X ${numTab}: ${numTab*9}\n`);
+}
